7db81a82b731296b956f83eef3b14ca725c5770b
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-04-26 01:54:13 +0200

    feat: otel support
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/roadrunner-server/gzip/v2
 
 go 1.18
 
-require github.com/klauspost/compress v1.15.1
+require (
+	github.com/klauspost/compress v1.15.1
+	github.com/roadrunner-server/sdk/v2 v2.13.3
+	go.opentelemetry.io/otel/trace v1.6.3
+)
+
+require (
+	github.com/roadrunner-server/errors v1.1.2 // indirect
+	github.com/roadrunner-server/tcplisten v1.1.2 // indirect
+	go.opentelemetry.io/otel v1.6.3 // indirect
+)
--- plugin.go
+++ plugin.go
@@ -4,9 +4,13 @@ import (
 	"net/http"
 
 	"github.com/klauspost/compress/gzhttp"
+	"github.com/roadrunner-server/sdk/v2/utils"
+	"go.opentelemetry.io/otel/trace"
 )
 
-const PluginName = "gzip"
+const (
+	PluginName = "gzip"
+)
 
 type Plugin struct{}
 
@@ -15,7 +19,16 @@ func (g *Plugin) Init() error {
 }
 
 func (g *Plugin) Middleware(next http.Handler) http.Handler {
-	return gzhttp.GzipHandler(next)
+	return gzhttp.GzipHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if val, ok := r.Context().Value(utils.OtelTracerNameKey).(string); ok {
+			tp := trace.SpanFromContext(r.Context()).TracerProvider()
+			ctx, span := tp.Tracer(val).Start(r.Context(), PluginName)
+			defer span.End()
+			next.ServeHTTP(w, r.WithContext(ctx))
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	}))
 }
 
 func (g *Plugin) Name() string {
