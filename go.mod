64c095bf8a5ed69ff28f7e002c1be8fc27023791
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-05-10 13:02:37 +0200

    feat: add otel schemaURL
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- go.mod
+++ go.mod
@@ -5,11 +5,16 @@ go 1.18
 require (
 	github.com/klauspost/compress v1.15.3
 	github.com/roadrunner-server/sdk/v2 v2.14.0
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.32.0
 	go.opentelemetry.io/otel v1.7.0
 	go.opentelemetry.io/otel/trace v1.7.0
 )
 
 require (
+	github.com/felixge/httpsnoop v1.0.2 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/roadrunner-server/errors v1.1.2 // indirect
 	github.com/roadrunner-server/tcplisten v1.1.2 // indirect
+	go.opentelemetry.io/otel/metric v0.30.0 // indirect
 )
--- plugin.go
+++ plugin.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/klauspost/compress/gzhttp"
 	"github.com/roadrunner-server/sdk/v2/utils"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel/propagation"
+	semconv "go.opentelemetry.io/otel/semconv/v1.10.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -27,11 +29,12 @@ func (g *Plugin) Middleware(next http.Handler) http.Handler {
 	return gzhttp.GzipHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if val, ok := r.Context().Value(utils.OtelTracerNameKey).(string); ok {
 			tp := trace.SpanFromContext(r.Context()).TracerProvider()
-			ctx, span := tp.Tracer(val).Start(r.Context(), PluginName)
+			ctx, span := tp.Tracer(val, trace.WithSchemaURL(semconv.SchemaURL), trace.WithInstrumentationVersion(otelhttp.SemVersion())).Start(r.Context(), PluginName, trace.WithSpanKind(trace.SpanKindServer))
 			defer span.End()
 
-			r = r.WithContext(ctx)
+			// inject
 			g.prop.Inject(r.Context(), propagation.HeaderCarrier(r.Header))
+			r = r.WithContext(ctx)
 		}
 
 		next.ServeHTTP(w, r)
